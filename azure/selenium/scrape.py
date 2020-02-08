2f86d77652ce21c489cad6aa0042dd447646c6e3
astrotyan <astrotyan@gmail.com>
2020-02-08 08:51:20 -0500

    add try/except in azure/selenium/scrape.py for each url

--- azure/selenium/scrape.py
+++ azure/selenium/scrape.py
@@ -30,55 +30,58 @@ driver = webdriver.Chrome()
 
 with open(query+'.csv', 'a') as csvfile:
     jobs_writer = csv.writer(csvfile)
-    for i, url in enumerate(urls):
+    for i, url in enumerate(urls[1875:]):
         print('Scraping {i} out of {n} jobs'.format(i=i+1, n=len(urls)))
-        driver.get(url)
-        job = {}
-        # column_names = ['title', 'location', 'job_id', 'date', 'travel', 'profession', 'role', 'employment', 'description', 'responsibilites', 'qualifications']
-        # title
-        wait_job = WebDriverWait(driver, 10)
-        title = wait_job.until(EC.presence_of_element_located(
-            (By.XPATH,
-             '//*[@id="content-1"]/div[1]/div/h1')))
-        job['title'] = title.text
-        # location
         try:
-            time.sleep(.5)
-            location = [driver.find_element_by_xpath(
-                '//*[@id="content-1"]/div[1]/div/div/span').text]
+            driver.get(url)
+            job = {}
+            # column_names = ['title', 'location', 'job_id', 'date', 'travel', 'profession', 'role', 'employment', 'description', 'responsibilites', 'qualifications']
+            # title
+            wait_job = WebDriverWait(driver, 10)
+            title = wait_job.until(EC.presence_of_element_located(
+                (By.XPATH,
+                 '//*[@id="content-1"]/div[1]/div/h1')))
+            job['title'] = title.text
+            # location
+            try:
+                time.sleep(.5)
+                location = [driver.find_element_by_xpath(
+                    '//*[@id="content-1"]/div[1]/div/div/span').text]
+            except:
+                location_button = driver.find_element_by_xpath(
+                    '//*[@id="content-1"]/div[1]/div/div/button')
+                location_button.click()
+                time.sleep(.5)
+                location = []
+                locs = driver.find_elements_by_xpath(
+                    '//*[@id="content-1"]/div[1]/div/ul/li')
+                for loc in locs:
+                    location.append(loc.text)
+            job['job_id'] = driver.find_element_by_xpath(
+                '//*[@id="content-1"]/div[3]/div/ul/li[1]/span[2]').text
+            job['date'] = driver.find_element_by_xpath(
+                '//*[@id="content-1"]/div[3]/div/ul/li[2]/span[2]').text
+            job['travel'] = driver.find_element_by_xpath(
+                '// *[@id="content-1"]/div[3]/div/ul/li[3]/span[2]').text
+            job['profession'] = driver.find_element_by_xpath(
+                '//*[@id="content-1"]/div[3]/div/ul/li[4]/span[2]').text
+            job['role'] = driver.find_element_by_xpath(
+                '//*[@id="content-1"]/div[3]/div/ul/li[5]/span[2]').text
+            job['employment'] = driver.find_element_by_xpath(
+                '//*[@id="content-1"]/div[3]/div/ul/li[6]/span[2]').text
+            job['description'] = driver.find_element_by_xpath(
+                '//*[@id="content-1"]/div[4]/div[1]').text
+            job['responsibilites'] = driver.find_element_by_xpath(
+                '//*[@id="content-1"]/div[4]/div[2]/p').text
+            job['qualifications'] = driver.find_element_by_xpath(
+                '//*[@id="content-1"]/div[4]/div[3]/p').text
+            for loc in location:
+                job['location'] = loc
+                jobs_writer.writerow(job.values())
+            if job['title'] == '':
+                print('Warning: ', job['job_id'], ' has no title!')
         except:
-            location_button = driver.find_element_by_xpath(
-                '//*[@id="content-1"]/div[1]/div/div/button')
-            location_button.click()
-            time.sleep(.5)
-            location = []
-            locs = driver.find_elements_by_xpath(
-                '//*[@id="content-1"]/div[1]/div/ul/li')
-            for loc in locs:
-                location.append(loc.text)
-        job['job_id'] = driver.find_element_by_xpath(
-            '//*[@id="content-1"]/div[3]/div/ul/li[1]/span[2]').text
-        job['date'] = driver.find_element_by_xpath(
-            '//*[@id="content-1"]/div[3]/div/ul/li[2]/span[2]').text
-        job['travel'] = driver.find_element_by_xpath(
-            '// *[@id="content-1"]/div[3]/div/ul/li[3]/span[2]').text
-        job['profession'] = driver.find_element_by_xpath(
-            '//*[@id="content-1"]/div[3]/div/ul/li[4]/span[2]').text
-        job['role'] = driver.find_element_by_xpath(
-            '//*[@id="content-1"]/div[3]/div/ul/li[5]/span[2]').text
-        job['employment'] = driver.find_element_by_xpath(
-            '//*[@id="content-1"]/div[3]/div/ul/li[6]/span[2]').text
-        job['description'] = driver.find_element_by_xpath(
-            '//*[@id="content-1"]/div[4]/div[1]').text
-        job['responsibilites'] = driver.find_element_by_xpath(
-            '//*[@id="content-1"]/div[4]/div[2]/p').text
-        job['qualifications'] = driver.find_element_by_xpath(
-            '//*[@id="content-1"]/div[4]/div[3]/p').text
-        for loc in location:
-            job['location'] = loc
-            jobs_writer.writerow(job.values())
-        if job['title'] == '':
-            print('Warning: ', job['job_id'], ' has no title!')
+            print('ERROR!')
         time.sleep(random.randint(1, 3))
 
 driver.close()
