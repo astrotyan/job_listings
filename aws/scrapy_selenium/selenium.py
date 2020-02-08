33be1d8d622645d1664d787b9b212fb6d1216e4c
astrotyan <astrotyan@gmail.com>
2020-02-08 10:53:09 -0500

    rearange file structure

--- aws/scrapy_selenium/selenium.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-
-driver = webdriver.Chrome()
-
-driver.get('https://www.amazon.jobs/en/search?base_query=aws&loc_query=')
-
-driver.find_element_by_xpath('//*[@class="col-sm-6 job-count-info"]').text
-
-driver.close()
